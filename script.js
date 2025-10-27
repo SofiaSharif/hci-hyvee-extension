480612a0a1b5677b0ba4314ba544bd9793db7edd
ssharif3 <174224547+SofiaSharif@users.noreply.github.com>
2025-10-27 10:49:18 -0500

    Added multi screen functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+let currentScreen = document.getElementById("screen main");
+showScreen("screen main");
+
+function showScreen(screenId) {
+    currentScreen.classList.remove("active");
+    currentScreen = document.getElementById(screenId);
+    currentScreen.classList.add("active");
+}
